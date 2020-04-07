c2028daafdd52c61feb605697cad20ec7b72314e
huangyisan <anonymousyisan@gmail.com>
2020-04-07 16:13:33 +0800

    fix v-for add bind key

--- src/views/detail/childComponents/DetailSwiper.vue
+++ src/views/detail/childComponents/DetailSwiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <swiper class="swiper-item" >
-      <swiper-item v-for="item in topImages">
+      <swiper-item v-for="(item, index) in topImages" :key="index">
         <img :src="item" alt="" v-lazy="item">
       </swiper-item>
     </swiper>
